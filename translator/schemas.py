4dfbeaad8a9f0812a6746bb5591211dade4fc7f5
João Natividade <87316339+joaosnet@users.noreply.github.com>
2024-09-18 22:01:34 -0300

    feat: Adicionar validação de objeto antes do campo PyObjectId
    Adiciona a validação de objeto antes do campo PyObjectId para garantir que apenas valores válidos sejam aceitos.

--- translator/schemas.py
+++ translator/schemas.py
@@ -3,6 +3,10 @@ from typing import Dict, List
 
 from bson import ObjectId
 from pydantic import BaseModel, Field
+from pydantic.functional_validators import BeforeValidator
+from typing_extensions import Annotated
+
+PyObjectId = Annotated[str, BeforeValidator(str)]
 
 
 class Message(BaseModel):
@@ -21,22 +25,6 @@ class TranslateSchema(BaseModel):
 
 class TranslatefordbSchema(BaseModel):
     comentario: str
-
-
-class PyObjectId(ObjectId):
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, v):
-        if not ObjectId.is_valid(v):
-            raise ValueError('Invalid objectid')
-        return ObjectId(v)
-
-    @classmethod
-    def __get_pydantic_json_schema__(cls, field_schema):
-        field_schema.update(type='string')
 
 
 class ConfigDict:
